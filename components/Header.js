c2447e7d3e9c6dbdc5f3ec62ff66dceffd42d081
Noah Snelson <noah.snelson@protonmail.com>
2021-03-16 21:50:37 -0700

    Start on GetOutThere site

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Header() {
+    return (
+        <Head>
+            <title>Get Out There</title>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="shortcut icon" type="image/x-icon" href="https://assets.squarespace.com/universal/default-favicon.ico"/>
+        </Head>
+    );
+}
