cfa1a927e19f287913b416d1a99d2bed6400d52f
Noah Snelson <noah.snelson@protonmail.com>
2021-04-08 23:50:18 -0700

    Properly fetch and render cart server-side via cookies

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,24 +1,33 @@
-import Cookies from 'js-cookie'
+import { parse } from 'cookie'
 import Header from '../components/Header'
 
-export default function Cart() {
-    const cartItems = getCartItems().map( item =>
-        <li>
-            {item.title} - ${item.price.toFixed(2)}
-        </li>
-    );
+export default function Cart({ cartItems }) {
+    const cartListElements = Object.values(cartItems).map( props => {
+        const product = JSON.parse(props);
+        return (
+            <li>
+                {product.title} - ${product.price.toFixed(2)}
+            </li>
+        );
+    });
+
     return (
         <div>
           <Header />
           <ul>
-            {cartItems}
+            {cartListElements}
           </ul>
         </div>
     );
 }
 
-function getCartItems(){
-    return Cookies.get();
+export async function getServerSideProps(context){
+    const cookie = context.req.headers.cookie;
+    const cookieObject = parse(cookie);
+
+    return { props: {
+        cartItems: cookieObject
+    }};
 }
 
 export function addItemToCart(props){
