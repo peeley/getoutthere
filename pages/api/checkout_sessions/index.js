1d1269b58b86ced077b02966a49fd5c1c764b945
Noah Snelson <noah.snelson@protonmail.com>
2021-05-10 23:39:53 -0700

    Correctly return checkout session from API

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -1,26 +1,33 @@
 import { Stripe } from 'stripe'
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
     const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY, {
         apiVersion: '2020-03-02',
     });
 
-    const line_items = JSON.parse(req.body).items.map( item => { return {
-        name: item.title,
-        amount: formatAmountForStripe(item.price, "USD"),
-        currency: "usd",
-        quantity: item.quantity
-    }});
+    const line_items = JSON.parse(req.body).items.map(item => {
+        return {
+            name: item.title,
+            amount: formatAmountForStripe(item.price, "USD"),
+            currency: "usd",
+            quantity: item.quantity
+        }
+    });
 
     const params = {
         submit_type: 'pay',
         payment_method_types: ['card'],
         line_items,
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/cart`,
     }
 
-    const checkoutSession = await stripe.checkout.sessions.create(params);
+    try {
+        const checkoutSession = await stripe.checkout.sessions.create(params);
+        res.status(200).json(checkoutSession);
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
 function formatAmountForStripe(amount, currency) {
@@ -34,7 +41,7 @@ function formatAmountForStripe(amount, currency) {
     let zeroDecimalCurrency = true
     for (let part of parts) {
         if (part.type === 'decimal') {
-        zeroDecimalCurrency = false
+            zeroDecimalCurrency = false
         }
     }
     return zeroDecimalCurrency ? amount : Math.round(amount * 100)
