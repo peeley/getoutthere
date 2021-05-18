ce3f8ac8de3c126b0571cfa07e9fc0abf3f3931e
Noah Snelson <noah.snelson@protonmail.com>
2021-05-17 22:00:31 -0700

    Formatting, more styling changes for mobile

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ import { Context } from '../lib/State'
 
 export default function Header() {
 
-    const [cart, ] = useContext(Context);
+    const [cart,] = useContext(Context);
 
     let totalItems = 0;
     const getTotalCartItems = cart.forEach((item) => {
@@ -19,39 +19,39 @@ export default function Header() {
                 <title>Get Out There</title>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="shortcut icon" type="image/x-icon" href="https://assets.squarespace.com/universal/default-favicon.ico"/>
+                <link rel="shortcut icon" type="image/x-icon" href="https://assets.squarespace.com/universal/default-favicon.ico" />
             </Head>
-            <div className="flex flex-row justify-between m-16 text-2xl">
+            <div className="flex flex-row justify-between m-16 text-md md:text-2xl">
                 <div className="flex flex-row">
-                    <h2 className="pr-7">
+                    <h2 className="pr-3 md:pr-7">
                         <Link href="/shop">
-                        <a>Shop</a>
+                            <a>Shop</a>
                         </Link>
                     </h2>
                     <h2 className="pl-7">
                         <Link href="/our-story">
-                        <a>Our Story</a>
+                            <a>Our Story</a>
                         </Link>
                     </h2>
                 </div>
 
-                <h1 className="justify-self-center text-5xl mr-20 font-bold"><Link href="/"><a>Get Out There</a></Link></h1>
+                <h1 className="justify-self-center text-lg md:text-5xl mr-20 font-bold"><Link href="/"><a>Get Out There</a></Link></h1>
 
                 <div className="flex flex-row">
                     <div className="mr-7">
                         <Link href="https://www.instagram.com/getouthere907">
                             <a>
-                                <Image className="content-center" src="/insta-logo.svg" height="25" width="25"/>
+                                <Image className="content-center" src="/insta-logo.svg" layout="fixed" height="25" width="25" />
                             </a>
                         </Link>
                     </div>
-                    <div>
+                    <div class="flex row">
                         <Link href="/cart">
                             <a>
-                                <Image src="/cart-icon.svg" height="30" width="30"/>
+                                <Image src="/cart-icon.svg" height="30" layout="fixed" width="30" />
                             </a>
                         </Link>
-                        <span className="align-text-top text-base mt-5">{ totalItems > 0 ? totalItems : null }</span>
+                        <span className="align-text-top text-base mt-5">{totalItems > 0 ? totalItems : null}</span>
                     </div>
                 </div>
             </div>
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,78 +6,80 @@ import { loadStripe } from '@stripe/stripe-js'
 
 export default function Cart() {
 
-    const [state, dispatch] = useContext(Context);
+  const [state, dispatch] = useContext(Context);
 
-    const renderCartItems = (items) =>
-        items.map( (product, index) => {
-            // FIXME table might not be best for mobile view
-            return(
-                <tr className="border-t" key={index}>
-                    <td className="flex flex-row justify-start ml-5 items-center">
-                        <button className="hover:text-gray-900 hover:bg-gray-300 rounded text-gray-500 bg-gray-200 py-2 px-4 mx-5 my-10 align-middle" onClick={() => dispatch({ type: 'REMOVE_ITEM', index })}>&times;</button>
-                      <Image src={product.imagePath} width="100" height="100"/>
-                      <span className="ml-5 font-bold text-2xl flex-grow content-center">{product.title}</span>
-                    </td>
-                    <td className="text-right">
-                      <input className="border w-1/6 text-center" min="1" type="number" value={product.quantity}
-                             onChange={(e) => dispatch({ type: 'CHANGE_QUANTITY', index, newQuantity: parseInt(e.target.value) })} />
-                    </td>
-                    <td className="font-bold text-2xl text-center">${product.price.toFixed(2) * product.quantity}</td>
-                </tr>
-            );
-        });
+  const renderCartItems = (items) =>
+    items.map((product, index) => {
+      // FIXME table might not be best for mobile view
+      return (
+        <tr className="border-t" key={index}>
+          <td className="flex flex-row justify-start md:ml-5 items-center">
+            <button className="hover:text-gray-900 hover:bg-gray-300 rounded text-gray-500 bg-gray-200 py-2 px-4 md:mx-5 my-10 align-middle" onClick={() => dispatch({ type: 'REMOVE_ITEM', index })}>&times;</button>
+            <Image src={product.imagePath} width="100" height="100" />
+            <span className="ml-5 font-bold text-2xl flex-grow content-center">{product.title}</span>
+          </td>
+          <td className="text-right">
+            <input className="border w-1/6 text-center" min="1" type="number" value={product.quantity}
+              onChange={(e) => dispatch({ type: 'CHANGE_QUANTITY', index, newQuantity: parseInt(e.target.value) })} />
+          </td>
+          <td className="font-bold text-2xl text-center">${product.price.toFixed(2) * product.quantity}</td>
+        </tr>
+      );
+    });
 
-    let subtotal = state.reduce( (sum, item) => sum + (item.price * item.quantity), 0 );
+  let subtotal = state.reduce((sum, item) => sum + (item.price * item.quantity), 0);
 
-    const submitToCheckout = async (event) => {
-        event.preventDefault();
+  const submitToCheckout = async (event) => {
+    event.preventDefault();
 
-        const checkoutResponse = await fetch(
-            '/api/checkout_sessions',
-            { method: 'POST',
-              body: JSON.stringify({
-                items: state,
-              })}
-        );
+    const checkoutResponse = await fetch(
+      '/api/checkout_sessions',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          items: state,
+        })
+      }
+    );
 
-        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-        const checkoutSession = await checkoutResponse.json();
+    const checkoutSession = await checkoutResponse.json();
 
-        const { error } = await stripe.redirectToCheckout({
-            sessionId: checkoutSession.id
-        });
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id
+    });
 
-        alert(error.message);
-    }
+    alert(error.message);
+  }
 
-    const cartItems = state.length > 0
+  const cartItems = state.length > 0
     ? <>
-        <table className="table-fixed w-full">
-          <thead>
-            <tr>
-                <th className="text-center pl-16">Item</th>
-                <th className="text-right">Quantity</th>
-                <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            { renderCartItems(state) }
-          </tbody>
-        </table>
-        <div className="my-10 text-right">
-          <span className="block font-bold text-2xl">Subtotal: ${subtotal.toFixed(2)}</span>
-            <button className="text-xl font-bold bg-blue-600 text-white rounded-full my-5 p-5" onClick={submitToCheckout}>Checkout</button>
-        </div>
-      </>
+      <table className="table-fixed w-full">
+        <thead>
+          <tr>
+            <th className="text-center pl-16">Item</th>
+            <th className="text-right">Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderCartItems(state)}
+        </tbody>
+      </table>
+      <div className="my-10 text-right">
+        <span className="block font-bold text-xl md:text-2xl">Subtotal: ${subtotal.toFixed(2)}</span>
+        <button className="text-lg md:text-xl font-bold bg-blue-600 text-white rounded-full my-5 p-3 md:p-5" onClick={submitToCheckout}>Checkout</button>
+      </div>
+    </>
     : <p className="text-2xl font-bold">Your cart is empty. <Link href="/shop"><a className="underline">Start Shopping</a></Link></p>
 
-    return (
-        <div>
-          <div className="mb-16 mt-20 mx-16">
-            <h1 className="text-3xl font-bold mb-10">SHOPPING CART</h1>
-            { cartItems }
-          </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="mb-16 mt-20 mx-16">
+        <h1 className="text-3xl font-bold mb-10">SHOPPING CART</h1>
+        {cartItems}
+      </div>
+    </div>
+  );
 }
